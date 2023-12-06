ec3e386d65bdc188309a90eb04546c927c471b92
Mikyla Raquinio <micu.raquinio.up@phinmaed.com>
2023-12-06 14:24:17 +0800

    initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "MikylaRaquinio-Block4-Quiz"
+include(":app")
+ 
